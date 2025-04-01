965bce87cfeccf2168936e2a8e7309001fa3f5a2
george-gioseffi <georgegioseffi@hotmail.com>
2025-04-01 05:00:22 -0300

    adiciona comentário e mensagem de saudação no main

--- src/OlaTimoteoX.java
+++
@@ -1,5 +0,0 @@
-public class OlaTimoteoX {
-        public static void main(String[] args){
-                System.out.println("Calourada-Med-Vr");
-        }
-}
---
+++ src/academy/devdojo/maratonajava/introducao/OlaTimoteoX.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+
+public class OlaTimoteoX {
+        //Isso é um comentário de uma linha
+        /*
+                Isso é um comentário
+                de multiplas
+                linhas
+         */
+        public static void main(String[] args){
+                System.out.println("Calourada-Med-Vr");
+        }
+}
